0f3f4e07da347e9f4a0ce114136481003d0e8d6e
ng1n3 <code.with.muyiwa@gmail.com>
2024-02-06 20:24:56 +0100

    rerendering the item lists

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <Logo />
       <Form />
       <PackingList />
@@ -12,6 +18,7 @@ export default function App() {
 function Logo() {
   return <h1>🌴Far Away 🎒</h1>;
 }
+
 function Form() {
   return (
     <div className="add-form">
@@ -19,12 +26,33 @@ function Form() {
     </div>
   );
 }
+
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>🎒 You have X items on your list, and you already packed X (X%)</em>
     </footer>
   );
