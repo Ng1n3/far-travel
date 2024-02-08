fa38072bcf405097bfce05b1f887a7ddf8de72d4
ng1n3 <code.with.muyiwa@gmail.com>
2024-02-08 21:50:05 +0100

    moving components into separate files

---
+++ src/components/PackingList.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import  Item  from "./App";
+
+export default function PackingList({
+  items,
+  onDeleteItem,
+  onToggleItems,
+  onResetItems,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(b.packed) - Number(a.packed));
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            key={item.id}
+            onToggleItems={onToggleItems}
+          />
+        ))}
+      </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onResetItems}>Clear list</button>
+      </div>
+    </div>
+  );
+}
